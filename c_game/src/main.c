d22b7dfe7f561824eebe43372012dce426548389
Santiago Verdu <erra@qmake.org>
2026-09-06 16:27:01 -0600

    l

--- c_game/src/main.c
+++ c_game/src/main.c
@@ -1,15 +1,23 @@
 #include <flecs.h>
 #include <stdio.h>
 
+typedef struct {
+  int range;
+  long bps;
+  int version;
+} BLE, WFD, UWB;
+
 int main() {
   ecs_world_t *world = ecs_init();
-  printf("Nihao...\n");
 
-  ECS_COMPONENT(Brain);
+  ECS_COMPONENT(world, BLE);
+  ECS_COMPONENT(world, WFD);
+  ECS_COMPONENT(world, UWB);
 
   ecs_entity_t brian = ecs_new(world);
-  ecs_add(world, brian, Brain);
+  ecs_add(world, brian, BLE);
 
   ecs_fini(world);
+  printf("Nihao...\n");
   return 0;
 }
