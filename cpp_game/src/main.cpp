0f35e58f6eb4aae4ff9e1f29e9f993ce7fc6fe24
Santiago Verdu <erra@qmake.org>
2026-09-24 01:34:18 -0600

    make

--- cpp_game/src/main.cpp
+++ cpp_game/src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <chrono>
 
-int main(int arc, char** argv) {
-  std::cout << "Hi";
+int main(int argc, char** argv) {
+  using Clock = std::chrono::steady_clock;
+  auto t = Clock::now();
+
+  int ticks = 10;
+  while (ticks-- > 0) {
+    auto dt = Clock::now();
+    double frameTime = (dt-t).count();
+    std::cout << frameTime << "\n";
+    t = dt;
+  }
+
   return 0;
 }
